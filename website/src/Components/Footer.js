7a2396d4e73f3aa4bef87cde7f4256ca447eccb6
mugumbyabenon <106659839+mugumbyabenon@users.noreply.github.com>
2023-07-08 23:01:03 +0300

    Update Footer.js

--- website/src/Components/Footer.js
+++ website/src/Components/Footer.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
- 
+
   return (
     <div className="contactus">
     <footer class="footer">
