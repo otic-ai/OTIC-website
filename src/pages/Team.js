e6c92df1764d74eacaf65896ae008460ecb205ae
MUSHANA <mugumbyabenon03@gmail.com>
2023-07-13 17:54:39 +0300

    test

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -13,7 +13,7 @@ const Team = () => {
         <div style={{height:'20px'}}></div>
        <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Nesta Katende</div>
        <div style={{height:'10px'}}></div>
-       Our esteemed CEO, a visionary leader with a proven track record of driving success. With extensive industry expertise and a strategic mindset, they spearhead our organization towards excellence.
+       Our esteemed CEO, a visionary leader with a proven track record of driving success. With extensive industry expertise and a strategic mindset,  spearheading our organization towards excellence.
        <div style={{height:'1.0rem'}}></div>
        <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
        <div style={{width:'1.5rem'}}></div>
@@ -102,7 +102,46 @@ const Team = () => {
   	 			</div>
            <div style={{height:'0.5rem'}}></div>
       </div>  
-        </div>         
+        </div>  
+
+         <div className='content'>   
+      <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
+        <img style={{marginLeft:'10%'}} src='owen.png' ></img>
+        <div style={{height:'20px'}}></div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Mwine Owen    </div>
+       <div style={{height:'10px'}}></div>
+       Business Development Specialist
+       <div style={{height:'1.0rem'}}></div>
+       <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
+       <div style={{width:'1.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faFacebook} style={{color:'#999999',}} /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+            <a href="#"><FontAwesomeIcon icon={faTwitter}  style={{color:'#999999'}}  /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faLinkedin}  style={{color:'#999999'}}  /></a>
+  	 			</div>
+           <div style={{height:'0.5rem'}}></div>
+      </div>  
+        </div>   
+        <div className='content'>   
+      <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
+        <img style={{marginLeft:'10%'}} src='benon.png' ></img>
+        <div style={{height:'20px'}}></div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Benon Mugumya    </div>
+       <div style={{height:'10px'}}></div>
+       Data Engineer
+       <div style={{height:'1.0rem'}}></div>
+       <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
+       <div style={{width:'1.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faFacebook} style={{color:'#999999',}} /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+            <a href="#"><FontAwesomeIcon icon={faTwitter}  style={{color:'#999999'}}  /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faLinkedin}  style={{color:'#999999'}}  /></a>
+  	 			</div>
+           <div style={{height:'0.5rem'}}></div>
+      </div>  
+        </div>               
        </div>
     </section>
   )
