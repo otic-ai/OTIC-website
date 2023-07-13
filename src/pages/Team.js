d1ba3900a13189bf976e635506f124bc9f56ef8f
MUSHANA <mugumbyabenon03@gmail.com>
2023-07-13 17:47:12 +0300

    test

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -28,11 +28,11 @@ const Team = () => {
         </div>   
         <div className='content'>   
       <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
-        <img style={{marginLeft:'10%'}} src='divblogentrythumb@2x.png' ></img>
+        <img style={{marginLeft:'10%'}} src='marvin.png' ></img>
         <div style={{height:'20px'}}></div>
-       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Nesta Katende</div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Marvin Ssemakula  </div>
        <div style={{height:'10px'}}></div>
-       Our esteemed CEO, a visionary leader with a proven track record of driving success. With extensive industry expertise and a strategic mindset, they spearhead our organization towards excellence.
+       Finance Analyst
        <div style={{height:'1.0rem'}}></div>
        <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
        <div style={{width:'1.5rem'}}></div>
@@ -46,7 +46,63 @@ const Team = () => {
       </div>  
         </div>  
       
-        
+        <div className='content'>   
+      <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
+        <img style={{marginLeft:'10%'}} src='mark.png' ></img>
+        <div style={{height:'20px'}}></div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Mark Arinda </div>
+       <div style={{height:'10px'}}></div>
+       Software & AI Engineer
+       <div style={{height:'1.0rem'}}></div>
+       <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
+       <div style={{width:'1.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faFacebook} style={{color:'#999999',}} /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+            <a href="#"><FontAwesomeIcon icon={faTwitter}  style={{color:'#999999'}}  /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faLinkedin}  style={{color:'#999999'}}  /></a>
+  	 			</div>
+           <div style={{height:'0.5rem'}}></div>
+      </div>  
+        </div>     
+        <div className='content'>   
+      <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
+        <img style={{marginLeft:'10%'}} src='cyrus.png' ></img>
+        <div style={{height:'20px'}}></div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Cyrus Muwanguzi  </div>
+       <div style={{height:'10px'}}></div>
+       UX/UI & Digital Marketing Specialist
+       <div style={{height:'1.0rem'}}></div>
+       <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
+       <div style={{width:'1.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faFacebook} style={{color:'#999999',}} /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+            <a href="#"><FontAwesomeIcon icon={faTwitter}  style={{color:'#999999'}}  /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faLinkedin}  style={{color:'#999999'}}  /></a>
+  	 			</div>
+           <div style={{height:'0.5rem'}}></div>
+      </div>  
+        </div>   
+        <div className='content'>   
+      <div style={{maxWidth:'85%',fontSize:'0.825rem',fontWeight:'400', marginLeft:'1.5rem' }}>
+        <img style={{marginLeft:'10%'}} src='king.png' ></img>
+        <div style={{height:'20px'}}></div>
+       <div style={{fontSize:'0.95rem', fontWeight:'500'}}>Kibuuka Micheal Edwin   </div>
+       <div style={{height:'10px'}}></div>
+       Machine Learning Engineer
+       <div style={{height:'1.0rem'}}></div>
+       <div class="social-links" style={{display:'flex', flexDirection:'row'}}>
+       <div style={{width:'1.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faFacebook} style={{color:'#999999',}} /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+            <a href="#"><FontAwesomeIcon icon={faTwitter}  style={{color:'#999999'}}  /></a>
+  	 				<div style={{width:'0.5rem'}}></div>
+  	 				<a href="#"><FontAwesomeIcon icon={faLinkedin}  style={{color:'#999999'}}  /></a>
+  	 			</div>
+           <div style={{height:'0.5rem'}}></div>
+      </div>  
+        </div>         
        </div>
     </section>
   )
